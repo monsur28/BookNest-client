011475f661ed5891210e809f40cc4c81aeaaf386
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-19 02:13:40 +0600

    updated

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -103,15 +103,6 @@ const Login = () => {
         </div>
       </div>
     </div>
-
-
-const Login = () => {
-    return (
-        <div>
-            <h1>Login</h1>
-        </div>
-
-    );
-};
+)};
 
 export default Login;
