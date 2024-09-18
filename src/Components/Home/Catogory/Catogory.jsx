a982c398d6ecf24099fc6937e5cb5b632dba65fb
deveshbis <deveshbiswas.cse@gmail.com>
2024-09-18 17:38:13 +0600

    updated category section

--- src/Components/Home/Catogory/Catogory.jsx
+++ src/Components/Home/Catogory/Catogory.jsx
@@ -17,11 +17,11 @@ const Catogory = () => {
   }, []);
 
   return (
-    <div>
+    <div className='mb-5'>
       <h1 className="text-xl lg:text-4xl font-semibold text-black mb-2">Catogory</h1>
 
       <Swiper
-        
+
         slidesPerView={4}
         centeredSlides={true}
         spaceBetween={30}
@@ -33,16 +33,23 @@ const Catogory = () => {
         className="mySwiper"
       >
 
-{
-            catrgory.map(cat=> <SwiperSlide key={cat.image}>
-              <img src={cat.image} className='w-72 h-[300px] mb-5 hover:scale-125 transition-transform' alt="" />
-              </SwiperSlide>)
-          }
-        
-          
-        
+        {
+          catrgory.map(cat => <SwiperSlide key={cat.image}>
+            <div className="flex flex-col items-center">
+            <img src={cat.image} className="lg:w-72 lg:h-[300px] w-96 h-40 mb-5 hover:scale-105 transition-transform" alt="Book 1"/>
+            <div className="text-center">
+                <p className="font-semibold">{cat.name}</p>
+            </div>
+        </div>
+
+
+          </SwiperSlide>)
+        }
+
+
+
       </Swiper>
-    </div>
+    </div >
   );
 };
 
