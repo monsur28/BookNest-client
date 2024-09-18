df57166735ade076c5268ea89a2e87826028fe65
Md Fahim Hossain <fahimreshad3@gmail.com>
2024-09-19 02:42:33 +0600

    updated login page

--- src/Page/Login/Login.jsx
+++ src/Page/Login/Login.jsx
@@ -1,6 +1,5 @@
-
 import Marquee from "react-fast-marquee";
-import { Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { useForm } from "react-hook-form";
 
 const Login = () => {
@@ -10,43 +9,38 @@ const Login = () => {
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
+  const onSubmit = (data) => {};
 
-  };
-
-
-    return (
-        <div className="flex items-center justify-center p-6">
+  return (
+    <div className="flex items-center justify-center p-6">
       <div className="flex flex-col lg:flex-row h-full w-full overflow-hidden rounded-xl shadow-md md:h-[90%] md:w-[80%] lg:h-[70%]">
         {/* register design side  */}
-        <div className="relative h-full items-center justify-center bg-[#F2D184CC] md:flex md:w-[100%] lg:w-[40%]">
+        <div className="relative h-full items-center justify-center bg-[#F65D4E] md:flex md:w-[100%] lg:w-[40%]">
           <div className="space-y-2 text-center">
-            <h2 className="md:text-4xl lg:text-5xl font-medium text-[#F65D4E] ">
+            <h2 className="md:text-4xl lg:text-5xl font-medium text-white pt-4">
               Now
             </h2>
-            <h2 className="animate-pulse text-3xl md:text-5xl lg:text-7xl font-semibold text-[#F65D4E] ">
-              Find your life partner
+            <h2 className="animate-pulse text-3xl md:text-5xl lg:text-7xl font-semibold text-white">
+              Find your Book Here
             </h2>
-            <p className="animate-pulse text-xl md:text-3xl lg:text-4xl font-poppins font-semibold text-[#F65D4E] pt-6">
+            <p className="animate-pulse text-xl md:text-3xl lg:text-4xl font-poppins font-semibold text-white pt-6">
               Easy and fast.
             </p>
 
             <Marquee>
               <img
                 className="w-full lg:pt-20"
-                src="https://i.ibb.co/4TxsdDM/login-bg.png"
+                src="https://i.ibb.co.com/ySRSNtB/vector-bookshelf-made-wood-books-260nw-2223713083-removebg-preview.png"
                 alt=""
               />
             </Marquee>
           </div>
         </div>
         {/* input side  */}
-        <div className="flex w-full flex-col justify-center bg-white lg:w-[60%] space-y-6 mt-4">
-          <p className="-mb-4 text-center text-xl font-semibold text-[#F65D4E]">
-            START FOR FREE
-          </p>
-          <h2 className=" text-center text-3xl font-bold text-[#F65D4E]">
-            Sign in to Matrimony
+        <div className="flex w-full flex-col justify-center shadow-2xl shadow-orange-50 lg:w-[60%] space-y-6 my-4">
+          <p className="-mb-4 text-center text-xl font-bold">START FOR FREE</p>
+          <h2 className=" text-center text-3xl font-bold">
+            Sign in to BookNest
           </h2>
           <form
             onSubmit={handleSubmit(onSubmit)}
@@ -54,7 +48,7 @@ const Login = () => {
           >
             <input
               {...register("email", { required: true })}
-              className="w-[80%] rounded-lg border border-[#F65D4E] px-6 py-2 focus:outline-none focus:ring-2 focus:ring-[#F65D4E]/50 lg:w-[60%]"
+              className="w-[80%] rounded-lg border px-6 py-2 focus:outline-none focus:ring-2 focus:ring-[#F65D4E]/50 lg:w-[60%]"
               type="email"
               placeholder="Email"
               name="email"
@@ -64,7 +58,7 @@ const Login = () => {
             )}
             <input
               {...register("password", { required: true })}
-              className="w-[80%] rounded-lg border border-[#F65D4E] px-6 py-2 focus:outline-none focus:ring-2 focus:ring-[#F65D4E]/50 lg:w-[60%]"
+              className="w-[80%] rounded-lg border px-6 py-2 focus:outline-none focus:ring-2 focus:ring-[#F65D4E]/50 lg:w-[60%]"
               type="password"
               placeholder="Password"
               name="password"
@@ -72,9 +66,12 @@ const Login = () => {
             {errors.password && (
               <span className="text-red-600">Password is required</span>
             )}
-            <p className="text-[14px] text-[#F65D4E]">
+            <p className="text-[14px] font-medium">
               Do not have an account ?{" "}
-              <Link to="/registration" className="text-[#F65D4E] font-bold">
+              <Link
+                to="/registration"
+                className="text-[#F65D4E] text-[16px] font-bold"
+              >
                 Registration Now
               </Link>
             </p>
@@ -91,9 +88,7 @@ const Login = () => {
             <hr className="flex-1" />
           </div>
           {/* sign with google */}
-          <div
-            className="mx-auto flex h-[50px] w-[200px] gap-2 items-center overflow-hidden rounded-full shadow-md duration-300 hover:scale-95 hover:shadow hover:cursor-pointer"
-          >
+          <div className="mx-auto flex h-[50px] w-[200px] gap-2 items-center overflow-hidden rounded-full shadow-md duration-300 hover:scale-95 hover:shadow hover:cursor-pointer">
             <div className="flex h-full w-[50%] items-center bg-[#F65D4E] pl-4 text-sm text-white">
               Sign With
             </div>
@@ -103,6 +98,7 @@ const Login = () => {
         </div>
       </div>
     </div>
-)};
+  );
+};
 
 export default Login;
